bb758b57e994850716a4bb03d8c6c597ea15d14c
melimarquezz <mmelina@udesa.edu.ar>
2024-04-08 17:18:11 -0300

    Cambio de nomenclatura a cammelCase

---
+++ myapp/routes/products.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', /*aca va el controlador*/ ); //  se va a relacionar con product.ejs 
+router.get('/add', /*aca va el controlador*/ ); //  se va a relacionar con productAdd.ejs 
+
+  
+  module.exports = router; 
