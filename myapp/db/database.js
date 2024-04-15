6e247968310c27fda77d6d0ce3e295e704196f11
melimarquezz <mmelina@udesa.edu.ar>
2024-04-15 18:23:42 -0300

    base de datos completa

--- myapp/db/database.js
+++ myapp/db/database.js
@@ -1,12 +1,16 @@
 const data = {
     usuario: {
-        nombre: "Nombre de usuario",
         email: "usuario@example.com",
-        pass: "123456"
+        usuario: "Nombre de usuario",
+        password: "123456",
+        fechaDeNacimiento: "16/05/2005",
+        documento: "46701774",
+        fotoDePerfil: "/public/images/users/defaultImage.png"
     },
     productos: [
         {
-            nombre: "Samsung Galaxy S23 Fe",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Samsung Galaxy S23 Fe",
             descripcion: "Fotografía profesional en tu bolsillo. Descubrí infinitas posibilidades para tus fotos con las 3 cámaras principales de tu equipo. Poné a prueba tu creatividad y jugá con la iluminación, diferentes planos y efectos para obtener grandes resultados.",
             // Otros datos del producto 1
 
@@ -20,7 +24,8 @@ const data = {
             ]
         },
         {
-            nombre: "Samsung Galaxy Z Flip5",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Samsung Galaxy Z Flip5",
             descripcion: "Descubrí la pantalla plegable: al plegarse, adopta un tamaño pequeño y compacto que te resultará muy cómodo para transportar. Cuando quieras disfrutar de tus contenidos favoritos se transforma desplegando su pantalla",
             // Otros datos del producto 2
             
@@ -34,7 +39,8 @@ const data = {
             ]
         },
         {
-            nombre: "Samsung Galaxy A04s 4",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Samsung Galaxy A04s 4",
             descripcion: "Descubrí infinitas posibilidades para tus fotos con las 3 cámaras principales de tu equipo. Poné a prueba tu creatividad y jugá con la iluminación, diferentes planos y efectos para obtener grandes resultados.",
             // Otros datos del producto 1
             
@@ -48,7 +54,8 @@ const data = {
             ]
         },
         {
-            nombre: "Motorola Edge 40(eSIM)",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Motorola Edge 40(eSIM)",
             descripcion: "Doble cámara y más detalles. Sus 2 cámaras traseras de 50 Mpx/13 Mpx te permitirán tomar imágenes con más detalles y lograr efectos únicos como el famoso modo retrato de poca profundidad de campo.",
             // Otros datos del producto 1
             
@@ -62,7 +69,8 @@ const data = {
             ]
         },
         {
-            nombre: "Moto G54 Dual SIM 256 GB",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Moto G54 Dual SIM 256 GB",
             descripcion: "Con su potente procesador y memoria RAM de 8 GB tu equipo alcanzará un alto rendimiento con gran velocidad de transmisión de contenidos y ejecutará múltiples aplicaciones a la vez sin demoras.",
             // Otros datos del producto 1
             
@@ -76,7 +84,8 @@ const data = {
             ]
         },
         {
-            nombre: "Motorola Moto E22",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Motorola Moto E22",
             descripcion: "Su memoria RAM de 4 GB permite que tu smartphone funcione de manera fluida y sin demoras al realizar distintas tareas, jugar o navegar.",
             // Otros datos del producto 1
             
@@ -90,7 +99,8 @@ const data = {
             ]
         },
         {
-            nombre: "Apple iPhone 15 (128 GB)",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Apple iPhone 15 (128 GB)",
             descripcion: "El iPhone 15 viene con la Dynamic Island, cámara gran angular de 48 MP, entrada USB-C y un resistente vidrio con infusión de color en un diseño de aluminio.",
             // Otros datos del producto 1
             
@@ -104,7 +114,8 @@ const data = {
             ]
         },
         {
-            nombre: "Apple iPhone 11 (128 GB)",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Apple iPhone 11 (128 GB)",
             descripcion: "Graba videos 4K y captura retratos espectaculares y paisajes increíbles con el sistema de dos cámaras. Toma grandes fotos con poca luz gracias al modo Noche.",
             // Otros datos del producto 1
             
@@ -118,7 +129,8 @@ const data = {
             ]
         },
         {
-            nombre: "Apple iPhone 14 (128 GB)",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Apple iPhone 14 (128 GB)",
             descripcion: "El iPhone 14 viene con el sistema de dos cámaras más impresionante en un Iphone 14, para que tomes fotos espectaculares con mucha o poca luz. Y te da más tranquilidad gracias a una funcionalidad de seguridad que salva vidas.",
             // Otros datos del producto 1
             
@@ -132,7 +144,8 @@ const data = {
             ]
         },
         {
-            nombre: "Samsung Galaxy A32 128",
+            imagenDelProducto: "/public/images/products/defaultImage.png",
+            nombreDelProducto: "Samsung Galaxy A32 128",
             descripcion: "Son equipos que funcionan como nuevos y traen garantía por 3 meses. Pueden tener algún detalle estético y detalles muy pequeños en la pantalla que no afectan el funcionamiento.",
             // Otros datos del producto 1
             
