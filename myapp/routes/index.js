7f5425594db51491ea32c5c5e96158acc866fd27
melimarquezz <mmelina@udesa.edu.ar>
2024-04-19 17:41:16 -0300

    primer chequeo de prolijidad del código

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+var express           = require('express');
+var router            = express.Router();
 const indexController = require ("../controllers/indexController")
 
 /* GET home page. */
--- myapp/routes/products.js
+++ myapp/routes/products.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+var express              = require('express');
+var router               = express.Router();
 const productsController = require ("../controllers/productsController")
 
 /* GET home page. */
